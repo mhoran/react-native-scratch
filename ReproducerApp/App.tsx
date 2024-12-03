e4d1962b4b7c634844896da711b24e3753c73dc5
Matthew Horan <matt@matthoran.com>
2024-12-03 16:20:20 -0500

    Reproducer template

--- ReproducerApp/App.tsx
+++ ReproducerApp/App.tsx
@@ -1,118 +1,80 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+import * as React from 'react';
 import {
   SafeAreaView,
   ScrollView,
-  StatusBar,
   StyleSheet,
   Text,
-  useColorScheme,
+  TextInput,
+  TouchableOpacity,
   View,
 } from 'react-native';
+import {KeyboardAvoidingView} from './KeyboardAvoidingView';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+export default function () {
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <View style={styles.container}>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.topbar}>
+          <View style={styles.channelsButtonWrapper}>
+            <TouchableOpacity style={styles.topbarButton}>
+              <Text style={[styles.channelsButtonText]}>#</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+        <KeyboardAvoidingView style={styles.container}>
+          <ScrollView style={{flex: 1}} keyboardDismissMode="interactive" />
+          <View style={styles.bottomBox}>
+            <TextInput style={styles.inputBox} />
+          </View>
+        </KeyboardAvoidingView>
+      </SafeAreaView>
     </View>
   );
 }
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
-
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  bottomBox: {
+    paddingHorizontal: 10,
+    paddingVertical: 7.5,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    backgroundColor: '#333',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  inputBox: {
+    maxHeight: 60.5,
+    padding: 5,
+    justifyContent: 'center',
+    borderColor: 'gray',
+    backgroundColor: '#fff',
+    flex: 1,
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  topbar: {
+    flexDirection: 'row',
+    backgroundColor: '#333',
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingBottom: 10,
   },
-  highlight: {
-    fontWeight: '700',
+  channelsButtonWrapper: {
+    paddingLeft: 10,
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+  },
+  topbarButton: {
+    paddingVertical: 5,
+    paddingHorizontal: 5,
+  },
+  channelsButtonText: {
+    textAlign: 'center',
+    fontSize: 20,
+    fontFamily: 'Gill Sans',
+    color: '#eee',
+    fontWeight: 'bold',
+  },
+  container: {
+    flex: 1,
+    backgroundColor: '#333',
   },
 });
-
-export default App;
